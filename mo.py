76f0b172f097fcb2b56a45f5b73bba7f34935145
Alethea Butler <alethea@aletheabutler.com>
2013-01-02 20:37:26 -0500

    Multiple modes for filename output

--- mo.py
+++ mo.py
@@ -5,7 +5,7 @@ import shutil
 import argparse
 import mutagen
 
-def process_name(name, mode, max_length=30):
+def process_name(name, mode, max_length=None):
     if mode == 'none':
         return unicode(name)
     splitnames = name.split()
@@ -19,18 +19,32 @@ def process_name(name, mode, max_length=30):
     unix = u'-'.join(subname.lower() for subname in subnames)
     if mode == 'short' or (mode == 'mixed' and len(unix) > max_length):
         return u''.join(subname.lower()[0] for subname in subnames)
-    if mode == 'unix':
+    if mode == 'unix' or mode == 'mixed':
         return unix
 
 def main():
-    parser = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(prog="MO",
             description="Organize music files into directories.")
+    mode_group = parser.add_mutually_exclusive_group()
     parser.add_argument('sources', metavar='SOURCE', nargs="+",
-            help="Files to examine")
+            help='Files to examine')
     parser.add_argument('target', metavar='TARGET',
-            help="Destination for new directory structure")
+            help='Destination for new directory structure')
+    parser.set_defaults(mode='unix', max_length=None)
+    mode_group.add_argument('-c', '--clean', dest='mode',
+            action='store_const', const='clean', help='Strip \
+                    non-alphanumeric characters from filenames, leave \
+                    whitespace and uppercase characters')
+    mode_group.add_argument('-s', '--shorten', dest='mode',
+            action='store_const', const='short', help='Shorten filenames to \
+                    lowercase initials')
+    mode_group.add_argument('-m', '--mixed', dest='max_length', type=int,
+            metavar='LENGTH', help='Shorten filenames if longer than \
+                    LENGTH, use UNIX names otherwise')
 
     args = parser.parse_args()
+    if args.max_length != None:
+        args.mode = 'mixed'
 
     directories = set()
     filepairs = list()
@@ -43,9 +57,13 @@ def main():
         album = metadata['album'][0]
         title = metadata['title'][0]
         track = int(metadata['tracknumber'][0])
-        directory = os.path.join(args.target, artist, album)
+        directory = os.path.join(args.target, 
+                process_name(artist, args.mode, args.max_length), 
+                process_name(album, args.mode, args.max_length))
         directories.add(directory)
-        filename = '{0:02}'.format(track) + title + os.path.splitext(source)[1].lower()
+        filename = ('{0:02}'.format(track) + 
+                process_name(title, args.mode, args.max_length) + 
+                os.path.splitext(source)[1].lower())
         filepairs.append((source, os.path.join(directory, filename)))
 
     for directory in directories:
